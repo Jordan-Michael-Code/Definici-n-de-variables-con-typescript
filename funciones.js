10148112718601ea00db8e5a344261d7b420149d
unknown <jordanmichaeldelgado@gmail.com>
2024-08-09 21:55:16 -0500

    Variables

---
+++ funciones.js
@@ -0,0 +1,16 @@
+"use strict";
+var saludo = 'hola mundo typescript';
+saludo = 'cambio';
+var numero = 10;
+numero = 5;
+var cualquiera;
+cualquiera = 1;
+cualquiera = 'cualquiera';
+var PI = 3.14;
+function saludar() {
+    console.log('saludar adios');
+    console.log(numero);
+    console.log(cualquiera);
+    console.log(PI);
+}
+saludar();
---
+++ funciones.ts
@@ -0,0 +1,15 @@
+let saludo = 'hola mundo typescript';
+saludo = 'cambio';
+let numero: number = 10;
+numero = 5;
+let cualquiera: any;
+cualquiera = 1;
+cualquiera = 'cualquiera';
+const PI = 3.14;
+function saludar() {
+  console.log('saludar adios');
+  console.log(numero);
+  console.log(cualquiera);
+  console.log(PI);
+}
+saludar();
